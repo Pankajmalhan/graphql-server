0253c9f6ab7c42b56f21ae7f5f1e8a5e64333298
Pankaj Choudhary <choudhary.pankaj@tftus.com>
2019-01-20 07:09:29 +0530

    pankaj

--- acedmind/index.js
+++ acedmind/index.js
@@ -12,7 +12,7 @@ const path=require("path")
 const bodyParser=require('body-parser');
 const { buildSchema}=require('graphql');
 
-console.log("pankaj 1");
+console.log("pankaj 12");
 app.use(bodyParser.json())
 
 app.use('/graphql',graphqlHTTP({
